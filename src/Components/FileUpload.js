1ab3e0da34c79653b8c5b3840541562f790a716c
Rohan Paul <rohanpaul2@gmail.com>
2018-11-08 18:30:46 +0530

    completing the disk-storage multer upload branch

--- src/Components/FileUpload.js
+++ src/Components/FileUpload.js
@@ -22,7 +22,40 @@ class FileUpload extends Component {
     });
   }
 
+  // importAll() is to leverage webpack's require.context so I can dynamically import all the files in a directory
+  // https://webpack.js.org/guides/dependency-management/#require-context
+  // The syntax is as follows:
+  // require.context(directory, useSubdirectories = false, regExp = /^\.\//);
+  // so importAll() takes an object as its argument and returns an object where each key is the full-name of the file (and the "./" part taken-off from it). Like 1541668392919-file.pdf
+  importAll = result => {
+    let images = {};
+    result.keys().map((item, index) => {
+      return (images[item.replace("./", "")] = result(item));
+    });
+    return images;
+  };
+
   render() {
+    const webpackContext = require.context(
+      "../../uploads",
+      false,
+      /\.(png|jpe?g|svg|pdf|doc|odt)$/
+    );
+
+    /*
+    A> In above, I am requiring a context with files from the 'documents' directory that can be required with a request endings with .png .jpeg .svg etc .
+
+    B> And the final returned object ( after searching the directory ) from the require.context is passed as the argument to importAll()
+
+    C> So,console.log(webpackContext.keys()); will print the below
+
+     ["./1541656477973-file.pdf", "./1541671735212-file.jpeg"]
+
+     And all importAll() is doing is replacing the "./" with empty space and returing the 'images' array.
+
+ */
+
+    const images = this.importAll(webpackContext);
     return (
       <div className="bg-success">
         <Col xs="8">
@@ -43,7 +76,7 @@ class FileUpload extends Component {
                         <td>{document.document_id}</td>
                         <td>{document.description}</td>
                         <td>
-                          <a href={document.fileLink} target="_blank">
+                          <a href={images[`${document.path}`]} target="_blank">
                             View File
                           </a>
                         </td>
